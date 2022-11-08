be7776b03a06c9d3b03eda199010b015c26d80b0
d4vv <dawid@mejster.com>
2022-11-08 01:49:03 +0100

    zbierz-druzyne-img-fix

--- components/content/ZbierzDruzyneContent.js
+++ components/content/ZbierzDruzyneContent.js
@@ -2,7 +2,27 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styled from 'styled-components'
 import ScrollDownArrow from '../scrollDownArrow'
-import ZbierzDruzyne from '../../public/images/projects/zbierz-druzyne/Project.jpg'
+import ZbierzDruzyne_1 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_1.jpg'
+import ZbierzDruzyne_2 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_2.jpg'
+import ZbierzDruzyne_3 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_3.jpg'
+import ZbierzDruzyne_4 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_4.jpg'
+import ZbierzDruzyne_5 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_5.jpg'
+import ZbierzDruzyne_6 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_6.jpg'
+import ZbierzDruzyne_7 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_7.jpg'
+import ZbierzDruzyne_8 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_8.jpg'
+import ZbierzDruzyne_9 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_9.jpg'
+import ZbierzDruzyne_10 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_10.jpg'
+import ZbierzDruzyne_11 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_11.jpg'
+import ZbierzDruzyne_12 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_12.jpg'
+import ZbierzDruzyne_13 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_13.jpg'
+import ZbierzDruzyne_14 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_14.jpg'
+import ZbierzDruzyne_15 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_15.jpg'
+import ZbierzDruzyne_16 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_16.jpg'
+import ZbierzDruzyne_17 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_17.jpg'
+import ZbierzDruzyne_18 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_18.jpg'
+import ZbierzDruzyne_19 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_19.jpg'
+import ZbierzDruzyne_20 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_20.jpg'
+import ZbierzDruzyne_21 from '../../public/images/projects/zbierz-druzyne/Zbierz-Druzyne_21.jpg'
 
 const Section = styled.div`
   pointer-events: none;
@@ -32,8 +52,148 @@ const ZbierzDruzyneContent = () => (
       <MotionImage>
         <Image
           className="imageNew"
-          src={ZbierzDruzyne}
-          alt="Zbierz druzyne Project"
+          src={ZbierzDruzyne_1}
+          alt="Zbierz druzyne Project 1"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_2}
+          alt="Zbierz druzyne Project 2"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_3}
+          alt="Zbierz druzyne Project 3"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_4}
+          alt="Zbierz druzyne Project 4"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_5}
+          alt="Zbierz druzyne Project 5"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_6}
+          alt="Zbierz druzyne Project 6"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_7}
+          alt="Zbierz druzyne Project 7"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_8}
+          alt="Zbierz druzyne Project 8"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_9}
+          alt="Zbierz druzyne Project 9"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_10}
+          alt="Zbierz druzyne Project 10"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_11}
+          alt="Zbierz druzyne Project 11"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_12}
+          alt="Zbierz druzyne Project 12"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_13}
+          alt="Zbierz druzyne Project 13"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_14}
+          alt="Zbierz druzyne Project 14"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_15}
+          alt="Zbierz druzyne Project 15"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_16}
+          alt="Zbierz druzyne Project 16"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_17}
+          alt="Zbierz druzyne Project 17"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_18}
+          alt="Zbierz druzyne Project 18"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_19}
+          alt="Zbierz druzyne Project 19"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_20}
+          alt="Zbierz druzyne Project 20"
+          layout='fill'
+          placeholder="blur"
+        />
+        <Image
+          className="imageNew"
+          src={ZbierzDruzyne_21}
+          alt="Zbierz druzyne Project 21"
           layout='fill'
           placeholder="blur"
         />
